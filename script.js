fd66468b75ca2ee904cf140f61bed78125781349
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2025-04-09 22:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 //your JS code here. If required.
 const student = {
-	name: navin
+	name: "navin",
 }
+
+function getKeys(obj) {
+	return Object.keys(obj);
+}
+
+console.log(getKeys(student));
 
 Object.prototype.getKeys = function() {
   return Object.keys(this);
